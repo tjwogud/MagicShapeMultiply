2e9e428753c6e583f74257ce6df0b7d98ddf19c7
tjwogud <rlatjsal78@gmail.com>
2022-07-27 19:24:32 +0900

    add shortcut, chinese translation, editor+ compatibility

--- MagicShapeMultiply/Main.cs
+++ MagicShapeMultiply/Main.cs
@@ -4,10 +4,10 @@ using EditorTabLib;
 using EditorTabLib.Properties;
 using EditorTabLib.Utils;
 using HarmonyLib;
+using Localizations;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using UnityEngine;
 using UnityModManagerNet;
 
@@ -15,23 +15,60 @@ namespace MagicShapeMultiply
 {
     public static class Main
     {
-        public static UnityModManager.ModEntry.ModLogger Logger;
+        public static Settings Settings;
         public static UnityModManager.ModEntry ModEntry;
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        public static Localization Localization;
         public static Harmony harmony;
         public static bool IsEnabled = false;
 
         public static Sprite Icon { get; private set; }
 
-        public static float f = 0.25f;
+        private static bool changing = false;
 
         public static void Setup(UnityModManager.ModEntry modEntry)
         {
             ModEntry = modEntry;
             Logger = modEntry.Logger;
+            modEntry.OnUpdate = (_, _) =>
+            {
+                if (scnEditor.instance == null || scnEditor.instance.playMode)
+                    return;
+                if ((!Settings.ctrl || Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+                    && (!Settings.shift || Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                    && Input.GetKeyDown(Settings.key))
+                    MagicShape.Multiply();
+            };
+            modEntry.OnGUI = _ =>
+            {
+                GUILayout.BeginHorizontal();
+                Settings.ctrl = GUILayout.Toggle(Settings.ctrl, "Ctrl");
+                GUILayout.Label("+");
+                Settings.alt = GUILayout.Toggle(Settings.alt, "Alt");
+                GUILayout.Label("+");
+                Settings.shift = GUILayout.Toggle(Settings.shift, "Shift");
+                GUILayout.Label("+");
+                if (!changing && GUILayout.Button($"{Settings.key}"))
+                    changing = true;
+                else if (changing)
+                {
+                    GUILayout.Button(Localization["msm.gui.changing"]);
+                    Event e = Event.current;
+                    if (e.isKey && e.type == EventType.KeyDown)
+                    {
+                        Settings.key = e.keyCode;
+                        changing = false;
+                    }
+                }
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+            };
+            modEntry.OnSaveGUI = _ => {
+                Logger.Log("Saving Settings...");
+                Settings.Save(modEntry);
+                Logger.Log("Save Completed!");
+            };
             modEntry.OnToggle = OnToggle;
-            Logger.Log("Loading Localizations...");
-            Localizations.Load();
-            Logger.Log("Load Completed!");
             Logger.Log("Loading Icon...");
             string path = Path.Combine(modEntry.Path, "icon.png");
             if (File.Exists(path))
@@ -42,10 +79,16 @@ namespace MagicShapeMultiply
                 {
                     Icon = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
                     Logger.Log("Load Completed!");
-                    return;
+                    goto next;
                 }
             }
             Logger.Log("Can't load Icon! try reinstall the mod.");
+            return;
+        next:
+            Logger.Log("Loading Settings...");
+            Settings = UnityModManager.ModSettings.Load<Settings>(modEntry);
+            Logger.Log("Load Completed!");
+            Localization = Localization.Load(modEntry, "1QcrRL6LAs8WxJj_hFsEJa3CLM5g3e8Ya0KQlRKXwdlU", 61572944, (keyValue) => (TypeUtils.ReplaceClassName(keyValue.Item1), keyValue.Item2));
         }
 
         private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
@@ -120,57 +163,10 @@ namespace MagicShapeMultiply
                             enableIf: new Dictionary<string, string>() { { "changeShape", "Enabled" } }),
                         new Property_Button(
                             name: "multiply",
-                            action: () => {
-                                using (new SaveStateScope(scnEditor.instance, false, false, false))
-                                {
-                                    LevelEvent levelEvent = scnEditor.instance.settingsPanel.selectedEvent;
-                                    MagicShape.MultiplyType multiplyType = (MagicShape.MultiplyType)levelEvent.data["multiplyType"];
-                                    float bpm = (float)levelEvent.data["beatsPerMinute"];
-                                    float multiplier = (float)levelEvent.data["bpmMultiplier"];
-                                    MagicShape.MultiplyType setSpeedType = (MagicShape.MultiplyType)levelEvent.data["setSpeedType"];
-                                    MagicShape.Angle? direction = levelEvent.disabled["direction"] ? null : (MagicShape.Angle)levelEvent.data["direction"];
-                                    MagicShape.ShowEvent showEvent = (MagicShape.ShowEvent)levelEvent.data["showEvent"];
-                                    bool changeShape = (ToggleBool)levelEvent.data["changeShape"] == ToggleBool.Enabled;
-                                    int? angleCorrection = levelEvent.disabled["angleCorrection"] ? null : (int)levelEvent.data["angleCorrection"];
-                                    Tuple<int, Dictionary<string, object>> result;
-                                    switch (multiplyType)
-                                    {
-                                        case MagicShape.MultiplyType.Bpm: {
-                                            result = MagicShape.MultiplyWithBPM(bpm, setSpeedType, showEvent, direction);
-                                            break;
-                                        }
-                                        case MagicShape.MultiplyType.Multiplier: {
-                                            result = !changeShape
-                                                ? MagicShape.MultiplyWithMultiplier(multiplier, setSpeedType, showEvent, direction)
-                                                : MagicShape.MultiplyWithAngle(multiplier, angleCorrection, setSpeedType);
-                                            break;
-                                        }
-                                        default:
-                                            return;
-                                    }
-                                    switch (result.Item1)
-                                    {
-                                        case -1:
-                                            PopupUtils.Show(Localizations.GetString("msm.editor.dialog.error", out string value) ? value : string.Empty);
-                                            break;
-                                        case -2:
-                                            PopupUtils.Show(Localizations.GetString("msm.editor.dialog.selectionIsSingleOrNone", out value) ? value : string.Empty);
-                                            break;
-                                        case -3:
-                                            PopupUtils.ShowParams(Localizations.GetString("msm.editor.dialog.containsExceptionEvents", out value) ? value : string.Empty, (result.Item2["eventTypes"] as List<LevelEventType>).Select(type => RDString.Get("editor." + type.ToString())).ToArray());
-                                            break;
-                                        case -4:
-                                            PopupUtils.Show(Localizations.GetString("msm.editor.dialog.tooBigAngle", out value, result.Item2) ? value : string.Empty);
-                                            break;
-                                        case -5:
-                                            PopupUtils.Show(Localizations.GetString("msm.editor.dialog.invalidAngle", out value, result.Item2) ? value : string.Empty);
-                                            break;
-                                    }
-                                }
-                            },
+                            action: () => MagicShape.Multiply(),
                             key: "msm.editor.multiply")
                     },
-                    saveSetting: true,
+                    saveSetting: false,
                     onFocused: () => {
                         Patches.SelectedColorsPatch.blue = true;
                         DOTween.Kill("selectedColorTween", false);
